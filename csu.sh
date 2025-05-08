023384f1839a001dc9097d04aec79545f5c96b07
Pebble Real <pawel.chesterfield@gmail.com>
2025-05-08 21:41:23 +0100

    Update csu.sh

--- csu.sh
+++ csu.sh
@@ -1,11 +1,11 @@
 step1 () {
 	clear
-	read -n 1 -s -p "THIS WILL DESTROY THE HARD DRIVE!!!!! DO YOU WANT TO CONTINUE? [y/n] "
-	ehco ""
+	read -p "THIS WILL DESTROY THE HARD DRIVE!!!!! DO YOU WANT TO CONTINUE? [y/n] " -n 1 ans
+	echo ""
 	if [ $ans = "n" ]; then
 		exit
 	elif [ $ans = "y" ]; then
-		sudo parted /dev/sda --mklabel msdos
+		sudo parted /dev/sda -- mklabel msdos
 		sudo parted /dev/sda -- mkpart primary 1MB -8GB
 		sudo parted /dev/sda -- set 1 boot on
 		sudo parted /dev/sda -- mkpart primary linux-swap -8GB 100%
